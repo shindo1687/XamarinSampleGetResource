a6520a5a754fd27b15f34a68d2f21036dca34369
shindo <shindo1687@gmail.com>
2020-04-14 22:54:29 +0900

    初期コミット

---
+++ SampleGetResource/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SampleGetResource
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            String text = GetResourceFileData("SampleGetResource.SampleText.txt");
+            System.Console.WriteLine("text = " + text);
+        }
+
+        private String GetResourceFileData(String fileName)
+        {
+            var assembly = typeof(MainPage).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream(fileName);
+            using (var reader = new System.IO.StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
